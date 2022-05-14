60dadc93d7e5c3d69d69be01bc45e018e2e801f8
hehe.bu <hehe.bu@rakuten.com>
2022-05-14 17:32:10 +0800

    init php samples and add aggregation svc

---
+++ samples/php/aggregation_svc/index.php
@@ -0,0 +1,48 @@
+<?php
+
+$reqPath = strtok($_SERVER["REQUEST_URI"], '?');
+
+$reaHeaders = getallheaders();
+$xid = $reaHeaders['x_dbpack_xid'] ?? '';
+
+if (empty($xid)) {
+    die('xid is not provided!');
+}
+
+$aggregationSvc = new AggregationSvc();
+
+switch ($reqPath) {
+    case '/v1/order/create':
+         if ($aggregationSvc->CreateSo($xid, false)) {
+             http_response_code(200);
+             echo json_encode([
+                 'success' => true,
+                 'message' => 'success',
+             ]);
+         } else {
+             http_response_code(400);
+             echo json_encode([
+                 'success' => false,
+                 'message' => 'fail',
+             ]);
+         }
+        break;
+    case '/v1/order/create2':
+        if ($aggregationSvc->CreateSo($xid, true)) {
+            http_response_code(200);
+            echo json_encode([
+                'success' => true,
+                'message' => 'success',
+            ]);
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                'success' => false,
+                'message' => 'fail',
+            ]);
+        }
+        break;
+    default:
+        echo 'api not found';
+}
+
